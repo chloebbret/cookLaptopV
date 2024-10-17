dcaef13ed5556a11af5b4942684e0df799e3d105
Chloe <b.bernard@lyon.ort.asso.fr>
2024-10-17 23:17:36 +0200

    Accueil

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.png';
+import './styles/App.css';
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hello World</p>
+        <div className="sign">
+          <button className="signIn">Sign IN</button>
+          <button className="signUp">Sign UP</button>
+        </div>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Bienvenue sur Cook !</p>
+
+        <div className="goToSite">
+          <a href="./pages/Dashboard.js">Entrez -></a>
+        </div>
       </header>
     </div>
   );
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,13 @@
+import '../styles/Dashboard.css';
+
+function Dashboard() {
+  return (
+    <div>
+      <header>
+        <p>Hello World</p>
+      </header>
+    </div>
+  );
+}
+
+export default Dashboard;
